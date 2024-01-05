50d5669653162f252171a398d5c2e3f73f8ce130
shirinvatani <shirinvatani97@gmail.com>
2024-01-05 20:17:27 +0330

    full Screen App

--- app/src/main/java/com/example/mantra/MainActivity.kt
+++ app/src/main/java/com/example/mantra/MainActivity.kt
@@ -8,9 +8,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,18 +27,35 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.mantra.ui.theme.MantraTheme
+import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
+import com.example.mantra.ui.theme.MantraAppTheme
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent {
-            MantraTheme {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
-                Splashscreen(img = painterResource(id =R.drawable.splash))
+        setContent {
+            MantraAppTheme {
+
+
+                Scaffold(contentWindowInsets = WindowInsets(left = 0.dp,
+                    top=0.dp, bottom = 0.dp, right = 0.dp),
+                ) {paddingValues->
+
+                    //your child views padding:
+
+                    Box(modifier = Modifier
+                        .padding(paddingValues)
+                    ){
+                    }
+                    Splashscreen(img = painterResource(id =R.drawable.splash))
+                    }
 
 
 
@@ -69,7 +91,7 @@ fun MainScreen(){
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MantraTheme {
+    MantraAppTheme {
         Splashscreen(img = painterResource(id = R.drawable.splash))
 
     }
